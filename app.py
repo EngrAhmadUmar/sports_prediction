72d908103ad73edf2ec71776859c2a19710d4d35
Ahmad Ahmed <zcaptain2002@gmail.com>
2021-10-31 22:42:05 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,5 +1,8 @@
+# %%writefile app.py%
 import streamlit as st
 import pickle
+import openpyxl
+import xlrd
 import numpy as np
 import pandas as pd
 from sklearn.preprocessing import StandardScaler
@@ -21,21 +24,37 @@ def main():
     # display the front end aspect
     st.markdown(html_temp, unsafe_allow_html=True)
     default_value_goes_here = ""
-    ball_control = st.number_input("Please enter the players Ball Control Attribute", 0, 100000000, 0)
-    short_passing = st.number_input("Please enter the players Short Passing Attribute", 0, 100000000, 0)
-    dribbling = st.number_input("Please enter the players Dribbling Attribute", 0, 100000000, 0)
-    crossing = st.number_input("Please enter the players Crossing Attribute", 0, 100000000, 0)
-    curve = st.number_input("Please enter the players Curve Attribute", 0, 100000000, 0)
+    # ball_control = st.number_input("Please enter the players Ball Control Attribute", 0, 100000000, 0)
+    # short_passing = st.number_input("Please enter the players Short Passing Attribute", 0, 100000000, 0)
+    # dribbling = st.number_input("Please enter the players Dribbling Attribute", 0, 100000000, 0)
+    # crossing = st.number_input("Please enter the players Crossing Attribute", 0, 100000000, 0)
+    # curve = st.number_input("Please enter the players Curve Attribute", 0, 100000000, 0)
 
-    attributes = [ball_control, short_passing, dribbling, crossing, curve]
+    uploaded_file = st.file_uploader("Choose a XLSX file", type="xlsx")
 
+    global dataframe
+    if uploaded_file:
+        df = pd.read_excel(uploaded_file)
+        dataframe = df
+        # st.dataframe(df)
+        # st.table(df)
+
+    # attributes = [ball_control, short_passing, dribbling, crossing, curve]
+    #
     result = ""
-
-    #Display Results
+    #
+    # # Display Books
     if st.button("Predict"):
+      arr = dataframe.columns
+
+      for i in arr:
+          notnull = dataframe[i][dataframe[i].notnull()]
+          min = notnull.min()
+          dataframe[i].replace(np.nan, min, inplace=True)
+
       scaler = StandardScaler()
-      scaler.fit([attributes])
-      featureshost = scaler.transform([attributes])
+      scaler.fit(dataframe)
+      featureshost = scaler.transform(dataframe)
       prediction = model.predict(featureshost)
 
       result = prediction
